e78a951c6e7526b57139a8d82cb233739ed2bd14
RahulKathayat <rkathayat1103@gmail.com>
2023-08-31 17:19:50 +0530

    installing dependencies

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
 
-const PORT = process.env.EXPO_PUBLIC_PORT || 8000;
+dotenv.config();
+const PORT = process.env.PORT ;
 
 app.use(cors({
     origin: "*",
@@ -16,3 +18,20 @@ app.use(cors({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log("Error in Database connection", err);
+  });
+
+
+app.listen(PORT, () => {
+console.log(`Server running on port number ${PORT}`);
+});
