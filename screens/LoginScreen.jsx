25bfc5a4b370b4f4179d5f9a6a774fc6672824dc
RahulKathayat <rkathayat1103@gmail.com>
2023-08-31 23:09:51 +0530

    building UI

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,21 +1,164 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem("authToken");
+
+        if (token) {
+          navigation.replace("Home");
+        } else {
+          // token not found , show the login screen itself
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+   
+  const handleLogin = () => {
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:8000/login", user)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        AsyncStorage.setItem("authToken", token);
+
+        navigation.replace("Home");
+      })
+      .catch((error) => {
+        Alert.alert("Login Error", "Invalid email or password");
+        console.log("Login Error", error);
+      });
+  };
+  
   return (
-    <View style={{flex:1,backgroundColor:"#928bc2",padding:10,alignItems:"center"}}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
       <KeyboardAvoidingView>
-        <View style={{marginTop:100, justifyContent:"center" , alignItems:"center"}}>
-            <Text style={{ fontSize: 20, fontWeight: "600" }} >Login</Text>
-            <Text style={{ fontSize: 20, fontWeight: "600" , marginTop:15}} >Login to your Account</Text>
+        <View
+          style={{
+            marginTop: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color:"#4A55A2", fontSize: 20, fontWeight: "600" }}> Login </Text>
+          <Text style={{ fontSize: 20, fontWeight: "600", marginTop: 15 }}>
+            Login to your Account
+          </Text>
         </View>
+
+        <View style={{ marginTop: 50 }}>
+          <View>
+            <Text style={{ fontSize: 17, color: "gray", fontWeight: "600" }}>Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Enter your email ... "
+            />
+          </View>
+
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Password
+            </Text>
+
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                fontSize: email ? 18 : 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Password ..."
+            />
+          </View>
+
+          <Pressable
+            onPress={()=>{}}
+            style={{
+              width: 200,
+              backgroundColor: "#4A55A2",
+              padding: 15,
+              marginTop: 50,
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: 50,
+            }}
+          >
+             <Text
+              style={{
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Login
+            </Text>
+
+          </Pressable>
+
+          <Pressable
+            onPress={() => navigation.navigate("Register")}
+            style={{ marginTop: 15 }}
+          >
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Dont't have an account? Sign Up
+            </Text>
+          </Pressable>
+          
+        </View>
+
       </KeyboardAvoidingView>
+
     </View>
-    
-    
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- server/index.js
+++ server/index.js
@@ -35,3 +35,27 @@ mongoose
 app.listen(PORT, () => {
 console.log(`Server running on port number ${PORT}`);
 });
+
+
+const User = require("./models/user");
+const Message = require("./models/message");
+
+//endpoint for registration of the user
+
+app.post("/register", (req, res) => {
+  const { name, email, password, image } = req.body;
+
+  // create a new User object
+  const newUser = new User({ name, email, password, image });
+
+  // save the user to the database
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "User registered successfully" });
+    })
+    .catch((err) => {
+      console.log("Error registering user", err);
+      res.status(500).json({ message: "Error registering the user!" });
+    });
+});
